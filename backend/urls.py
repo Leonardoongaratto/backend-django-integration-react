2f050955b0309e9f3583d5a8c9a1aa10a9270502
Leonardoongaratto <leonardocongarattodev@gmail.com>
2022-05-18 17:27:46 -0300

    Creating Listing endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from .views import UserViewSet, GroupViewSet
-# from core.views import ListViewSet, ItemViewSet
+from core.views import ListViewSet
 # from rest_framework.authtoken import views 
 from django.contrib import admin
 from django.urls import path, include
@@ -8,7 +8,7 @@ from django.urls import path, include
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
-# router.register(r'list', ListViewSet, basename='list')
+router.register(r'list', ListViewSet, basename='list')
 # router.register(r'item', ItemViewSet)
 
 urlpatterns = [
