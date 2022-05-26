dbe5cf3f5e50eca87e2856256d9cb27af83c633b
Leonardoongaratto <leonardocongaratto1@gmail.com>
2022-05-26 17:23:03 -0300

    Created endpoint to genarate token

--- backend/urls.py
+++ backend/urls.py
@@ -13,5 +13,6 @@ router.register(r'item', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token, name='api-tokn-auth'),
     path('admin/', admin.site.urls),
 ]
