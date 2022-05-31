d9786e361551f14a2c9881b2293e2af1e469e907
Leonardoongaratto <leonardocongaratto1@gmail.com>
2022-05-31 17:53:16 -0300

    Adicionado o allowed_host

--- backend/settings/heroku.py
+++ backend/settings/heroku.py
@@ -8,7 +8,9 @@ DEBUG = env.bool("DEBUG", False)
 
 SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+# ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+ALLOWED_HOSTS = ['quiet-forest-14873.herokuapp.com']
 
 DATABASES = {
     "default": env.db(),
